a3d100db33b9bc572f68e4137cf36875b8b63473
steve.mueller <steve.mueller@dream-soft.de>
2024-09-16 12:42:34 +0200

    - add batchbooking for segmnet

--- lib/Fhp/Action/SendSEPATransfer.php
+++ lib/Fhp/Action/SendSEPATransfer.php
@@ -53,6 +53,7 @@ class SendSEPATransfer extends BaseAction
         $numberOfTransactions = $xmlAsObject->CstmrCdtTrfInitn->GrpHdr->NbOfTxs;
         $CtrlSum = round((float)$xmlAsObject->CstmrCdtTrfInitn->GrpHdr->CtrlSum, 2);
         $hasReqdExDates = false;
+        $batchBooking = false;
         foreach ($xmlAsObject->CstmrCdtTrfInitn?->PmtInf as $pmtInfo) {
             $CtrlSum += (float)$pmtInfo->CtrlSum;
             // Checks for both, <ReqdExctnDt>1999-01-01</ReqdExctnDt> and <ReqdExctnDt><Dt>1999-01-01</Dt></ReqdExctnDt>
@@ -60,6 +61,7 @@ class SendSEPATransfer extends BaseAction
                 $hasReqdExDates = true;
                 break;
             }
+            if (isset($pmtInfo->BtchBookg)) $batchBooking = (string)$pmtInfo->BtchBookg == 'true';
         }
 
         //NOW READ OUT, WICH SEGMENT SHOULD BE USED:
@@ -69,6 +71,7 @@ class SendSEPATransfer extends BaseAction
             $segmentID = 'HICMES';
             $segment = \Fhp\Segment\CME\HKCMEv1::createEmpty();
             $segment->summenfeld = Btg::create($CtrlSum);
+            $segment->einzelbuchungGewuenscht = $batchBooking;
         } elseif ($numberOfTransactions == 1 && $hasReqdExDates) {
 
             // Terminierte SEPA-Überweisung (Segment HKCSE / Kennung HICSES)
@@ -80,6 +83,7 @@ class SendSEPATransfer extends BaseAction
             $segmentID = 'HICCMS';
             $segment = \Fhp\Segment\CCM\HKCCMv1::createEmpty();
             $segment->summenfeld = Btg::create($CtrlSum);
+            $segment->einzelbuchungGewuenscht = $batchBooking;
         } else {
 
             //SEPA Einzelüberweisung (Segment HKCCS / Kennung HICCSS).
@@ -112,6 +116,7 @@ class SendSEPATransfer extends BaseAction
         $segment->kontoverbindungInternational = Kti::fromAccount($this->account);
         $segment->sepaDescriptor = $this->xmlSchema;
         $segment->sepaPainMessage = new Bin($this->painMessage);
+
         return $segment;
     }
 
