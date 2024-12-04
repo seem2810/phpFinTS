e8d66f60805b01520e7dd57a24e1d048f8862d77
steve.mueller <steve.mueller@dream-soft.de>
2024-12-04 15:20:49 +0100

    - fix send sepa Transfer return segments

--- lib/Fhp/Segment/CME/HICMEv1.php
+++ lib/Fhp/Segment/CME/HICMEv1.php
@@ -5,7 +5,7 @@ namespace Fhp\Segment\CME;
 use Fhp\Segment\BaseSegment;
 
 /**
- * Segment: SEPA Einzelüberweisung Parameter (Version 1)
+ * Segment: Einreichung terminierter SEPA-Sammelüberweisung bestätigen (Version 1)
  *
  * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
  * Section: C.10.2.1 c)
