b1bc50f1a22af81998ad8cb545c78baa4475b994
steve.mueller <steve.mueller@dream-soft.de>
2024-12-04 15:10:54 +0100

    - fix send sepa Transfer return segments

--- lib/Fhp/Segment/CCS/HICCSSv1.php
+++ lib/Fhp/Segment/CCS/HICCSSv1.php
@@ -2,7 +2,7 @@
 
 namespace Fhp\Segment\CCS;
 
-use Fhp\Segment\BaseGeschaeftsvorfallparameter;
+use Fhp\Segment\BaseSegment;
 
 /**
  * Segment: SEPA Einzelüberweisung Parameter (Version 1)
@@ -10,7 +10,7 @@ use Fhp\Segment\BaseGeschaeftsvorfallparameter;
  * @link https://www.hbci-zka.de/dokumente/spezifikation_deutsch/fintsv3/FinTS_3.0_Messages_Geschaeftsvorfaelle_2015-08-07_final_version.pdf
  * Section: C.10.2.1 c)
  */
-class HICCSSv1 extends BaseGeschaeftsvorfallparameter
+class HICCSSv1 extends BaseSegment
 {
     // No parameters.
 }
